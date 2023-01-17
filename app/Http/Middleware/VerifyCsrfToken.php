af4e39156df6416affc3913211b2299e5e999a4d
Lukas Bock <lukas.bock@uni-oldenburg.de>
2023-01-17 23:16:37 +0100

    fixed csrf for deploy

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        'stripe/*',
+        'api/*',
+        'login',
+        'logout',
+        'register',
     ];
 }
